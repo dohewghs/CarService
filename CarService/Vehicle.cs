32fa4c830ccccff20a938da59c14eeed245306b9
dohewghs <dohewghs@gmail.com>
2026-03-09 13:43:19 +0200

    Rename class Car to Vehile

---
+++ CarService/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal class Vehicle
+    {
+        private string brand;
+        private string model;
+        private int year;
+        private double basePrice;
+
+        private Engine engine;
+        
+    }
+
+    
+}
