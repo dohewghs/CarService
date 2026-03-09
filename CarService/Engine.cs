fde767d797a6bbdd0d2eb65854e53a224d507228
dohewghs <dohewghs@gmail.com>
2026-03-09 13:41:30 +0200

    add engine with volume and type

---
+++ CarService/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal class Engine
+    {
+        private double volume;
+        private EngineType type;
+
+        public Engine(double _volume = 0, EngineType _type = EngineType.petrol)
+        {
+            this.volume = _volume;
+            this.type = _type;
+        }
+    }
+
+    internal enum EngineType
+    {
+        petrol,
+        diesel,
+        hybrid,
+        electric
+    }
+}
