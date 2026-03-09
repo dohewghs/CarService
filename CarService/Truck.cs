1950d5207900c67d746a11329354a4e6e92faaa9
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-09 21:54:03 +0200

    add class Truck with constructor

---
+++ CarService/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal class Truck : Vehicle
+    {
+        double loadCapacity;
+
+        public Truck(Engine _engine, string _brand = "", string _model = "", int _year = 0, double _basePrice = 0, double _loadCapacity) :
+            base(_engine, _brand, _model, _year, _basePrice)
+        {
+            this.loadCapacity = _loadCapacity;
+        }
+    }
+}
