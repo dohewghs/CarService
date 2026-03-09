cbca7e6916baa6182a2d202ce3f75fe512abf4ec
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-09 22:34:06 +0200

    add EUCustoms calculator logics

---
+++ CarService/EUCustomsCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal class USCustomsCalculator : ICustomsCalculator
+    {
+        private const double fixedCustoms = 15;
+        private const double ecoTaxCoefitient = 0.15;
+        private const double taxCoefitient = 0.2;
+        public double CalculateCustoms(Vehicle vehicle)
+        {
+            double ecoTax = vehicle.GetEngine().GetVolume() * ecoTaxCoefitient;
+
+            double subTotal = ecoTax + fixedCustoms;
+
+            double tax = taxCoefitient * subTotal;
+
+            return tax + subTotal;
+        }
+    }
+}
