0d95356c98b2600bc6ee52ffc7cbc9a17fe95913
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-09 22:21:27 +0200

    add calculator interface

---
+++ CarService/ICustomsClearance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal interface ICustomsCalculator
+    {
+        double CalculateCustoms(Vehicle vehicle);
+    }
+}
