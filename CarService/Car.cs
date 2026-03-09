d65c199544aba1b179a7d685892d07a4355907fd
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-09 21:51:03 +0200

    add class car with constructor

---
+++ CarService/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal class Car : Vehicle
+    {
+        public Car(Engine _engine, string _brand = "", string _model = "", int _year = 0, double _basePrice = 0) : 
+            base(_engine, _brand, _model, _year, _basePrice)
+        {
+        }
+    }
+}
