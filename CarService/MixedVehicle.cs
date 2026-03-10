5fce504fe6e91d9c73898304694a48015172ca26
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-10 14:59:26 +0200

    Fix bugs in constructors

--- CarService/MixedVehicle.cs
+++ CarService/MixedVehicle.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace CarService
 {
-    internal class MixedVehicle
+    internal class MixedVehicle : Vehicle
     {
         double loadCapacity;
 
-        public MixedVehicle(string _brand = "", string _model = "", int _year = 0, double _basePrice = 0, double _loadCapacity, Engine _engine = null) :
-            base(_engine, _brand, _model, _year, _basePrice)
+        public MixedVehicle(string _brand = "", string _model = "", int _year = 0, double _basePrice = 0, double _loadCapacity = 0, Engine _engine = null) :
+            base(_brand, _model, _year, _basePrice, _engine)
         {
             this.loadCapacity = _loadCapacity;
         }
