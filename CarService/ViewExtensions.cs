e22dd0bac0e7578a62473f06a12b4ae80308f0d8
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-15 00:15:57 +0200

    extensions like ReadInt() InputOptionInRange

---
+++ CarService/ViewExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService
+{
+    internal static class ViewExtensions
+    {
+        public static int InputOptionInRange(this IView view, int low, int upp)
+        {
+            while (true)
+            {
+                int value = view.ReadInt("Your option: ");
+
+                if (low <= value && value <= upp)
+                {
+                    return value;
+                }
+
+                Console.WriteLine($"Enter a number in [{low}, {upp}]");
+            }
+
+        }
+        public static int ReadInt(this IView view, string text)
+        {
+            while (true)
+            {
+                view.DisplayMessage(text);
+                string input = view.GetUserInput("");
+                int value;
+                if (int.TryParse(input, out value))
+                {
+                    return value;
+                }
+                view.DisplayMessageEndl("Invalid input. Enter a number");
+            }
+        }
+
+
+    }
+}
