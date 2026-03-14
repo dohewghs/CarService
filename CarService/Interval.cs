d47f5ec8f0e6b47f609a5a768d3a1998a107f995
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-14 19:44:11 +0200

    interval [lower, upper] with method IsInInterval

---
+++ CarService/Interval.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService
+{
+    internal struct Interval
+    {
+        public double Lower { get; set; }
+        public double Upper { get; set; }
+
+        public Interval(int low = 0, int upp = 0)
+        {
+            this.Lower = low;
+            this.Upper = upp;
+        }
+        public bool IsInInterval(int value)
+        {
+            return this.Lower <= value && this.Upper >= value;
+        }
+
+        public bool IsInInterval(double value)
+        {
+            return this.Lower <= value && this.Upper >= value;
+        }
+    }
+}
