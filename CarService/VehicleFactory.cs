13bf322f74bc1cd4231198a09b77ea6b7e913cec
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-15 01:14:24 +0200

    factory to create vehicles

---
+++ CarService/VehicleFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService
+{
+    internal class VehicleFactory
+    {
+        public static Vehicle Create(string line)
+        {
+            string[] parts = line.Split('|');
+
+            Vehicle vehicle;
+
+            switch (parts[0].ToLower())
+            {
+                case "car":
+                    vehicle = new Car();
+                    break;
+                case "truck":
+                    vehicle = new Truck();
+                    break;
+                case "mixed":
+                    vehicle = new MixedVehicle();
+                    break;
+                default:
+                    vehicle = null;
+                    break;
+            }
+
+            if (vehicle == null)
+                throw new Exception("Unspecified car type");
+
+            vehicle.Read(parts);
+
+            return vehicle;
+        }
+    }
+}
