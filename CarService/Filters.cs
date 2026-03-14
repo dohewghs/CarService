93d013ae677789e5aba56255232cfb0253cc684c
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-14 19:45:03 +0200

    filters for car selection

---
+++ CarService/Filters.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CarService
+{
+    internal class Filters
+    {
+        private string brand;
+        private string model;
+        private Interval yearInterval;
+        private Interval priceInterval;
+        private Interval engVolumeInterval;
+        private EngineType engineType;
+        public Filters()
+        {
+            this.brand = string.Empty;
+            this.model = string.Empty;
+            this.yearInterval = new Interval(1990, 2010);
+            this.priceInterval = new Interval(0, 5000);
+            this.engVolumeInterval = new Interval(1000, 2000);
+            this.engineType = EngineType.petrol;
+        }
+
+        public void ShowFilters(string text)
+        {
+            Console.WriteLine(text);
+            Console.WriteLine($"Brand: {this.brand}");
+            Console.WriteLine($"Model: {this.model}");
+            Console.WriteLine($"Year: {this.yearInterval.Lower}-{this.yearInterval.Upper}");
+            Console.WriteLine($"Engine type: {this.engineType}");
+            Console.WriteLine($"Engine volume: {this.engVolumeInterval.Lower}-{this.engVolumeInterval.Upper}" );
+            Console.WriteLine($"Price: {this.priceInterval.Lower}-{this.priceInterval.Upper}");
+        }
+
+        public bool IsSuitable(Vehicle vehicle)
+        {
+            if (vehicle == null ||
+                vehicle.Engine.Type != this.engineType ||
+                !this.yearInterval.IsInInterval(vehicle.Year) ||
+                !this.priceInterval.IsInInterval(vehicle.BasePrice) ||
+                !this.engVolumeInterval.IsInInterval(vehicle.Engine.Volume)
+                )
+            {
+                return false;
+            }
+
+            if (this.model != string.Empty && this.model != vehicle.Model ||
+                this.brand != string.Empty && this.brand != vehicle.Brand
+                )
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void Change()
+        {
+            Console.Clear();
+
+            Console.WriteLine("1. Brand \n2. Model\n3. Year\n4. Engine type\n5. Engine volume\n6. Price");
+
+            int option = Reader.InputOptionInRange(1, 6);
+
+            switch (option)
+            {
+                case 1:
+                    EnterBrand();
+                    break;
+                case 2:
+                    EnterModel();
+                    break;
+                case 3:
+                    EnterYear();
+                    break;
+                case 4:
+                    EnterEngineType();
+                    break;
+                case 5:
+                    EnterVolume();
+                    break;
+                case 6:
+                    EnterPrice();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void EnterBrand()
+        {
+            Console.Write("Enter Brand: ");
+
+            this.brand = new string(Console.ReadLine());
+        }
+        private void EnterModel()
+        {
+            Console.Write("Enter model: ");
+            this.model = Console.ReadLine();
+        }
+        private void EnterYear()
+        {
+            Console.WriteLine("Enter Interval: ");
+            string str = Console.ReadLine();
+            string[] parts = str.Split(' ');
+            this.yearInterval.Lower = int.Parse(parts[0]);
+            this.yearInterval.Upper = int.Parse(parts[1]);
+        }
+        private void EnterEngineType()
+        {
+            Console.Write("Enter engine type: ");
+            this.engineType = Enum.Parse<EngineType>(Console.ReadLine());
+        }
+        private void EnterVolume()
+        {
+            Console.WriteLine("Enter Interval: ");
+            string str = Console.ReadLine();
+            string[] parts = str.Split(' ');
+            this.engVolumeInterval.Lower = int.Parse(parts[0]);
+            this.engVolumeInterval.Upper = int.Parse(parts[1]);
+        }
+        private void EnterPrice()
+        {
+            Console.WriteLine("Enter Interval: ");
+            string str = Console.ReadLine();
+            string[] parts = str.Split(' ');
+            this.priceInterval.Lower = int.Parse(parts[0]);
+            this.priceInterval.Upper = int.Parse(parts[1]);
+        }
+    }
+}
