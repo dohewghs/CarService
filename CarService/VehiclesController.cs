51d9b78ff6202f981a71fe6398a78fae64a60ea7
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-16 18:56:52 +0200

    move VehicleFactory and FehicleService in 1 file

--- CarService/VehiclesController.cs
+++ CarService/VehiclesController.cs
@@ -37,4 +37,29 @@ namespace CarService
             }
         }
     }
+
+
+    internal class VehicleService
+    {
+        private List<Vehicle> vehicles;
+
+        public VehicleService()
+        {
+            vehicles = new List<Vehicle>();
+        }
+        public List<Vehicle> GetAll() => vehicles;
+        public void LoadFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                vehicles.Add(VehicleFactory.Create(line));
+            }
+        }
+        public void Add(Vehicle vehicle) => vehicles.Add(vehicle);
+    }
 }
